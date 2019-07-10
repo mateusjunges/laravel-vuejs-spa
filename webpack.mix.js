6e0e7b19535e2cd43b2a6c73fe9f93a6af51dc34
Mateus Junges <contato@mateusjunges.com>
2019-07-10 02:11:06 -0300

    Added module aliases

--- webpack.mix.js
+++ webpack.mix.js
@@ -10,6 +10,16 @@ const mix = require('laravel-mix');
  | file for the application as well as bundling up all the JS files.
  |
  */
-
 mix.js('resources/js/app.js', 'public/js')
     .sass('resources/sass/app.scss', 'public/css');
+
+mix.extend('aliasConfig', new class {
+    webpackConfig(webpackConfig) {
+        webpackConfig.resolve.extensions.push('.js', '.json', '.vue');
+        webpackConfig.resolve.alias = {
+            'vue$': 'vue/dist/vue.esm.js',
+            '@components': __dirname + '/resources/js/components/'
+        };
+    }
+});
+mix.aliasConfig();
